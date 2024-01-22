c1c93dbae5aaf667a111dedd2d19bec687f298e4
Shreyeska <129605801+Shreyeska@users.noreply.github.com>
2024-01-22 19:32:18 +0545

    5

---
+++ 5.py
@@ -0,0 +1,7 @@
+# Program to convert celsius to fahrenheit
+
+tempCelsius = float(input("Enter your temperature in degrees celsius: "))
+
+tempFahrenheit = (tempCelsius * 9 / 5) + 32
+
+print(f"Your temperature is {tempFahrenheit}°F")
